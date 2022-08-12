d914cc9e6302ff80047d061e8cdef0cec5ec94ce
YottyHol <git@oholliday.co.uk>
2022-08-12 14:55:01 +0100

    export to CSV

--- admin/src/index.js
+++ admin/src/index.js
@@ -4,6 +4,7 @@ const config = require("config")
 const request = require("request")
 const axios = require("axios")
 
+const ObjectsToCsv = require('objects-to-csv');
 const app = express()
 
 app.use(bodyParser.json({limit: "10mb"}))
@@ -15,18 +16,19 @@ async function getHoldings()
     const investments = await axios.get(`${config.investmentsServiceUrl}/investments`);
     const companies = await axios.get(`${config.companyServiceUrl}/companies`);
   
-    investments.data.map(({firstName, lastName,investmentTotal, date, holdings }) => {
+    investments.data.map(({userId, firstName, lastName,investmentTotal, date, holdings }) => {
       holdings.map(({ id, investmentPercentage }) => {
           holdingId = id;
           const value = investmentTotal * investmentPercentage;
           const company = companies.data.find(({ id }) => id === holdingId).name
   
           finHoldings.push({
-              firstName,
-              lastName,
-              date,
-              holding: company,
-              value
+            User: userId,
+            FirstName: firstName,
+            "Last Name": lastName,
+            Date:  date,
+            Holding: company,
+            Value:value
           });
       });
   });
@@ -37,43 +39,24 @@ async function getHoldings()
 
 app.get("/investments/export", async (req, res) => {
   try{
-    res.send(await getHoldings())
+    var holdings = await getHoldings();
+    
+    // If you use "await", code must be inside an asynchronous function:
+    (async () => {
+      const csv = new ObjectsToCsv(holdings);
+
+      // Return the CSV file as string:
+      const data = (await csv.toString());
+      
+    await axios.post(`${config.investmentsServiceUrl}/investments/export`, {data});
+    res.send(200)
+    })();
   }
   catch(e) {
     console.error(e)
     res.send(500)
   }
 
-}),
-
-app.get("/investments/getHoldings", async (req, res) => {
-
-  try{
-  const investments = await axios.get(`${config.investmentsServiceUrl}/investments`);
-  const companies = await axios.get(`${config.companyServiceUrl}/companies`);
-
-  investments.data.map(({firstName, lastName,investmentTotal, date, holdings }) => {
-    holdings.map(({ id, investmentPercentage }) => {
-        holdingId = id;
-        const value = investmentTotal * investmentPercentage;
-        const company = companies.data.find(({ id }) => id === holdingId).name
-
-        finHoldings.push({
-            firstName,
-            lastName,
-            date,
-            holding: company,
-            value
-        });
-    });
-});
-
-  res.send(finHoldings)
-  } 
-  catch(e) {
-    console.error(e)
-    res.send(500)
-  }
 }),
 
 app.get("/investments/:id", (req, res) => {
