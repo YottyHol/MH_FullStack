8d974ea7568eff686bbe13635f84f1042a42e0fd
YottyHol <git@oholliday.co.uk>
2022-08-12 14:19:38 +0100

    getHoldings

--- admin/src/index.js
+++ admin/src/index.js
@@ -2,10 +2,79 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const config = require("config")
 const request = require("request")
+const axios = require("axios")
 
 const app = express()
 
 app.use(bodyParser.json({limit: "10mb"}))
+
+finHoldings = []
+
+async function getHoldings()
+{
+    const investments = await axios.get(`${config.investmentsServiceUrl}/investments`);
+    const companies = await axios.get(`${config.companyServiceUrl}/companies`);
+  
+    investments.data.map(({firstName, lastName,investmentTotal, date, holdings }) => {
+      holdings.map(({ id, investmentPercentage }) => {
+          holdingId = id;
+          const value = investmentTotal * investmentPercentage;
+          const company = companies.data.find(({ id }) => id === holdingId).name
+  
+          finHoldings.push({
+              firstName,
+              lastName,
+              date,
+              holding: company,
+              value
+          });
+      });
+  });
+  
+    return finHoldings
+  
+}
+
+app.get("/investments/export", async (req, res) => {
+  try{
+    res.send(await getHoldings())
+  }
+  catch(e) {
+    console.error(e)
+    res.send(500)
+  }
+
+}),
+
+app.get("/investments/getHoldings", async (req, res) => {
+
+  try{
+  const investments = await axios.get(`${config.investmentsServiceUrl}/investments`);
+  const companies = await axios.get(`${config.companyServiceUrl}/companies`);
+
+  investments.data.map(({firstName, lastName,investmentTotal, date, holdings }) => {
+    holdings.map(({ id, investmentPercentage }) => {
+        holdingId = id;
+        const value = investmentTotal * investmentPercentage;
+        const company = companies.data.find(({ id }) => id === holdingId).name
+
+        finHoldings.push({
+            firstName,
+            lastName,
+            date,
+            holding: company,
+            value
+        });
+    });
+});
+
+  res.send(finHoldings)
+  } 
+  catch(e) {
+    console.error(e)
+    res.send(500)
+  }
+}),
 
 app.get("/investments/:id", (req, res) => {
   const {id} = req.params
@@ -17,7 +86,7 @@ app.get("/investments/:id", (req, res) => {
       res.send(investments)
     }
   })
-})
+}),
 
 app.listen(config.port, (err) => {
   if (err) {
